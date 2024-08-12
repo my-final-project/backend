a52a0699f42597a4966965e6f515e3456d5f03ce
Juliane Maran <julianemaran@gmail.com>
2024-08-12 18:31:59 -0300

    adiciona configuracoes docker

--- pet-flow-cloud/src/main/java/br/com/juhmaran/pet_flow_cloud/PetFlowCloudApplication.java
+++ pet-flow-cloud/src/main/java/br/com/juhmaran/pet_flow_cloud/PetFlowCloudApplication.java
@@ -2,12 +2,14 @@ package br.com.juhmaran.pet_flow_cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PetFlowCloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetFlowCloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetFlowCloudApplication.class, args);
+    }
 
 }
