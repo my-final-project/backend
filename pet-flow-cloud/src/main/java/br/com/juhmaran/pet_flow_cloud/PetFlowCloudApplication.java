f450fc5245a0e28b19ff309f5cbfefba690aa83a
Juliane Maran <juliane.maran@compasso.com.br>
2024-08-12 11:48:09 -0300

    initial project

---
+++ pet-flow-cloud/src/main/java/br/com/juhmaran/pet_flow_cloud/PetFlowCloudApplication.java
@@ -0,0 +1,13 @@
+package br.com.juhmaran.pet_flow_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFlowCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFlowCloudApplication.class, args);
+	}
+
+}
---
+++ pet-flow-cloud/src/test/java/br/com/juhmaran/pet_flow_cloud/PetFlowCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juhmaran.pet_flow_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFlowCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
